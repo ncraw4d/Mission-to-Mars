99a4fa19c68d2e38858c90ab3f130cb55b60eedd
ncraw4d <105513491+ncraw4d@users.noreply.github.com>
2022-08-03 13:25:53 -0700

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -19,7 +19,9 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": mars_hemi(browser), 
         "last_modified": dt.datetime.now()
+        
     }
 
     # Stop webdriver and return data
@@ -96,6 +98,24 @@ def mars_facts():
 
     ## Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def mars_hemi(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = [] 
+    for hemis in range(4):
+        
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[hemis].click()
+        element = browser.links.find_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
